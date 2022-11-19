d8736176cf80acbcd2046a9eab1f07f3ef4bbcec
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2022-11-19 11:38:35 -0500

    version history for event listener

---
+++ MixedRealityToolkit-Unity-mrtk3/UnityProjects/MRTKDevTemplate/Assets/Scripts/Variables.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class variables 
+{
+   public static bool whiteTurn=true;
+
+}
---
+++ MixedRealityToolkit-Unity-mrtk3/UnityProjects/MRTKDevTemplate/Assets/Scripts/eventListenerTurnMovement.cs
@@ -0,0 +1,35 @@
+//using System;
+//using System.Collections.Generic;
+//using Unity.Profiling;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
+//using UnityEngine.Serialization;
+//using UnityEngine.XR.Interaction.Toolkit;
+
+
+
+//public class eventListenerTurnMovement : MonoBehaviour
+//{
+//public static bool whiteTurn;
+ 
+  //   void Start()
+    //{
+// om = GetComponent<ObjectManipulator>();
+//print(gameObject.name);
+    
+//whiteTurn = true;
+
+
+  //  }
+
+    // Update is called once per frame
+  //  void Update(){
+
+   //     print(gameObject.name);
+  //  if (whiteTurn== true && gameObject.tag=="BlackPiece")
+   // whiteTurn = false;
+      //gameObject.GetComponent<ObjectManipulator>().enabled= false;
+  
+   // }}
+    
+
