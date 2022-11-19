3082eb182c90871610ae394dadf1eb25a11200c7
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2022-11-19 14:01:18 -0500

    2d array initialized for positions and planes added to represent the positions

--- MixedRealityToolkit-Unity-mrtk3/UnityProjects/MRTKDevTemplate/Assets/Scripts/eventListenerTurnMovement.cs
+++ MixedRealityToolkit-Unity-mrtk3/UnityProjects/MRTKDevTemplate/Assets/Scripts/eventListenerTurnMovement.cs
@@ -6,6 +6,52 @@
 //using UnityEngine.Serialization;
 //using UnityEngine.XR.Interaction.Toolkit;
 
+//using Microsoft.MixedReality.Toolkit.SpatialManipulation
+
+using System.Collections.Generic;
+
+using Microsoft.MixedReality.Toolkit.SpatialManipulation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class eventListenerTurnMovement : MonoBehaviour, IEventSystemHandler
+{
+    public static ObjectManipulator ob;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject.name == "BlackPiece")
+        {
+            print("IAM BOAT");
+        }
+        ob = GetComponent<ObjectManipulator>();
+    }
+
+    //OnSelectEntered
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Turn.TurnBool)
+        {
+            if (ob.IsActiveHovered == true)
+            {
+                print("BOAT  are hovered");
+                Turn.TurnBool = true;
+            }
+        }
+    }
+
+    public void BoatTurn()
+    {
+        print(" Hello the boats manipulation is done");
+        coral.ob.enabled = true;
+        ob.enabled = false;
+    }
+}
 
 
 //public class eventListenerTurnMovement : MonoBehaviour
